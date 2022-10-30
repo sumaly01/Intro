ec97b473cd6c7ae6a750eeee7182ffc6fbe7c255
sumaly01 <sumalybajra@gmail.com>
2022-10-30 21:57:38 +0545

    Client added

---
+++ Intro-client/src/views/pages/authentication/auth-forms/AuthRegister.js
@@ -0,0 +1,275 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation, gql, HttpLink, ApolloClient, InMemoryCache } from '@apollo/client';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import {
+    Box,
+    Button,
+    Checkbox,
+    Divider,
+    FormControl,
+    FormControlLabel,
+    FormHelperText,
+    Grid,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    Typography
+} from '@mui/material';
+
+// third party
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+// project imports
+import useConfig from 'hooks/useConfig';
+import useScriptRef from 'hooks/useScriptRef';
+import AnimateButton from 'ui-component/extended/AnimateButton';
+import { strengthColor, strengthIndicatorNumFunc } from 'utils/password-strength';
+
+// assets
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+// ===========================|| FIREBASE - REGISTER ||=========================== //
+
+const FirebaseRegister = () => {
+    const client = new ApolloClient({
+        link: new HttpLink({
+            uri: 'http://localhost:3000/graphql'
+        }),
+        cache: new InMemoryCache()
+    });
+    const theme = useTheme();
+    const scriptedRef = useScriptRef();
+    // const matchDownSM = useMediaQuery(theme.breakpoints.down('md'));
+    const { borderRadius } = useConfig();
+    const [showPassword, setShowPassword] = React.useState(false);
+    const [checked, setChecked] = React.useState(true);
+
+    const [strength, setStrength] = React.useState(0);
+    const [level, setLevel] = React.useState();
+    //  const { firebaseRegister, firebaseGoogleSignIn } = useAuth();
+
+    // const googleHandler = async () => {
+    //     try {
+    //         await firebaseGoogleSignIn();
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // };
+
+    const CREATE_ADMIN = gql`
+        mutation ($email: String!, $password: String!) {
+            createAdmin(createAdminInput: { email: $email, password: $password }) {
+                id
+                email
+            }
+        }
+    `;
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const changePassword = (value) => {
+        const temp = strengthIndicatorNumFunc(value);
+        setStrength(temp);
+        setLevel(strengthColor(temp));
+    };
+
+    useEffect(() => {
+        changePassword('123456');
+    }, []);
+    const { loading, error } = useMutation(CREATE_ADMIN);
+    if (loading) return 'Loading...';
+    if (error) return <pre>{error.message}</pre>;
+
+    return (
+        <>
+            <Grid container direction="column" justifyContent="center" spacing={2}>
+                {}
+            </Grid>
+
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: '',
+                    submit: null
+                }}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+                    password: Yup.string().max(255).required('Password is required')
+                })}
+                onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+                    try {
+                        console.log('values=>', values);
+                        const email = values.email;
+                        const password = values.password;
+                        console.log('datas=>', email, password);
+                        await client
+                            .mutate({
+                                variables: {
+                                    email,
+                                    password
+                                },
+                                mutation: CREATE_ADMIN
+                            })
+                            .then(
+                                () => {
+                                    window.location.href = '/login';
+                                },
+                                (err) => {
+                                    if (scriptedRef.current) {
+                                        setStatus({ success: false });
+                                        setErrors({ submit: err.message });
+                                        setSubmitting(false);
+                                    }
+                                }
+                            );
+                    } catch (err) {
+                        console.error(err);
+                        if (scriptedRef.current) {
+                            setStatus({ success: false });
+                            setErrors({ submit: err.message });
+                            setSubmitting(false);
+                        }
+                    }
+                }}
+            >
+                {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+                    <form noValidate onSubmit={handleSubmit}>
+                        <FormControl fullWidth error={Boolean(touched.email && errors.email)} sx={{ ...theme.typography.customInput }}>
+                            <InputLabel htmlFor="outlined-adornment-email-register">Email Address</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-email-register"
+                                type="email"
+                                value={values.email}
+                                name="email"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                inputProps={{}}
+                            />
+                            {touched.email && errors.email && (
+                                <FormHelperText error id="standard-weight-helper-text--register">
+                                    {errors.email}
+                                </FormHelperText>
+                            )}
+                        </FormControl>
+
+                        <FormControl
+                            fullWidth
+                            error={Boolean(touched.password && errors.password)}
+                            sx={{ ...theme.typography.customInput }}
+                        >
+                            <InputLabel htmlFor="outlined-adornment-password-register">Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password-register"
+                                type={showPassword ? 'text' : 'password'}
+                                value={values.password}
+                                name="password"
+                                label="Password"
+                                onBlur={handleBlur}
+                                onChange={(e) => {
+                                    handleChange(e);
+                                    changePassword(e.target.value);
+                                }}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                            edge="end"
+                                            size="large"
+                                        >
+                                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                inputProps={{}}
+                            />
+                            {touched.password && errors.password && (
+                                <FormHelperText error id="standard-weight-helper-text-password-register">
+                                    {errors.password}
+                                </FormHelperText>
+                            )}
+                        </FormControl>
+
+                        {strength !== 0 && (
+                            <FormControl fullWidth>
+                                <Box sx={{ mb: 2 }}>
+                                    <Grid container spacing={2} alignItems="center">
+                                        <Grid item>
+                                            <Box
+                                                style={{ backgroundColor: level?.color }}
+                                                sx={{ width: 85, height: 8, borderRadius: '7px' }}
+                                            />
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography variant="subtitle1" fontSize="0.75rem">
+                                                {level?.label}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            </FormControl>
+                        )}
+
+                        <Grid container alignItems="center" justifyContent="space-between">
+                            <Grid item>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={checked}
+                                            onChange={(event) => setChecked(event.target.checked)}
+                                            name="checked"
+                                            color="primary"
+                                        />
+                                    }
+                                    label={
+                                        <Typography variant="subtitle1">
+                                            Agree with &nbsp;
+                                            <Typography variant="subtitle1" component={Link} to="#">
+                                                Terms & Condition.
+                                            </Typography>
+                                        </Typography>
+                                    }
+                                />
+                            </Grid>
+                        </Grid>
+                        {errors.submit && (
+                            <Box sx={{ mt: 3 }}>
+                                <FormHelperText error>{errors.submit}</FormHelperText>
+                            </Box>
+                        )}
+
+                        <Box sx={{ mt: 2 }}>
+                            <AnimateButton>
+                                <Button
+                                    disableElevation
+                                    disabled={isSubmitting}
+                                    fullWidth
+                                    size="large"
+                                    type="submit"
+                                    variant="contained"
+                                    color="secondary"
+                                >
+                                    Sign up
+                                </Button>
+                            </AnimateButton>
+                        </Box>
+                    </form>
+                )}
+            </Formik>
+        </>
+    );
+};
+
+export default FirebaseRegister;
